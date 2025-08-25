4d1fac235ad1b06fe42910b04b1a873a020db2cd
berkay-eren <194712022+berkay-eren@users.noreply.github.com>
2025-08-25 15:37:41 +0300

    solve ex6-11

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, limit) {
+    if(limit < 0) return "ERROR";
+    let res = "";
+    for(let i = 0; i < limit; i++){
+        res += str;
+    }
+    return res;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,... args) {
+    const newarr = [];
+    array.forEach(element => {
+        if(!args.includes(element))
+            newarr.push(element);
+    });
+    return newarr;
 };
 
 // Do not edit below this line
